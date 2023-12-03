700825ac2eead0efed5fc4e00de8cbba1ae04bc3
jahre <ejahr@protonmail.com>
2023-12-03 18:52:13 +0100

    comment code of day 2

--- src/day02.rs
+++ src/day02.rs
@@ -7,10 +7,6 @@ fn read_input_to_string() -> String{
 }
 
 fn game_is_possible(game: &str) -> bool {
-    let max_red = 12;
-    let max_green = 13;
-    let max_blue = 14;
-
     // extract number of cubes and corresponding color
     let re = Regex::new(r"(\d+) (red|green|blue)").unwrap();
     let matches: Vec<_> = re.captures_iter(game).map(|m| {
@@ -21,18 +17,20 @@ fn game_is_possible(game: &str) -> bool {
     // check if any number of cubes exceeds corresponding max value
     for (number, color) in matches{
         match color {
-            "red" => if number.parse::<i32>().unwrap() > max_red {return false},
-            "green" => if number.parse::<i32>().unwrap() > max_green {return false},
-            "blue" => if number.parse::<i32>().unwrap() > max_blue {return false},
+            "red" => if number.parse::<i32>().unwrap() > 12 {return false},
+            "green" => if number.parse::<i32>().unwrap() > 13 {return false},
+            "blue" => if number.parse::<i32>().unwrap() > 14 {return false},
             _=> print!("no color found")
         }
     }
     return true
 }
 
-fn find_min_number_of_cubes(game: &str, color: &str) -> i32{
+fn max_number_of_cubes(game: &str, color: &str) -> i32 {
     let expression= ["([0-9]+) ", color].concat();
     let re = Regex::new(&expression[..]).unwrap();
+
+    // catch all occurrences of the expression and collect the numbers in a vec
     let cubes: Vec<_> = re.captures_iter(game).map(|m| {
             let (_, [number]) = m.extract();
             number.parse::<i32>().unwrap()
@@ -41,14 +39,11 @@ fn find_min_number_of_cubes(game: &str, color: &str) -> i32{
     *cubes.iter().max().unwrap()
 }
 
-fn power_of_fewest_number_of_cubes(game: &str) -> i32 {
-    let min_red_cubes = find_min_number_of_cubes(game, "red");
-    let min_green_cubes = find_min_number_of_cubes(game, "green");
-    let min_blue_cubes = find_min_number_of_cubes(game, "blue");
-
-    min_red_cubes * min_blue_cubes * min_green_cubes
+fn product_of_number_of_cubes(game: &str) -> i32 {
+     max_number_of_cubes(game, "red")
+        * max_number_of_cubes(game, "green")
+        * max_number_of_cubes(game, "blue")
 }
-
 
 pub fn solve_part1() -> i32 {
     let input = read_input_to_string();
@@ -69,7 +64,7 @@ pub fn solve_part2() -> i32 {
 
     let mut sum = 0;
     for game in input.lines(){
-        sum += power_of_fewest_number_of_cubes(game);
+        sum += product_of_number_of_cubes(game);
     }
     sum
 }
