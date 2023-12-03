faa0bcd5770a94f5728827c47a3bb6592e35041a
jahre <ejahr@protonmail.com>
2023-12-03 11:18:06 +0100

    solve day 1 part 2

--- src/day01.rs
+++ src/day01.rs
@@ -6,49 +6,43 @@ fn read_input_to_string() -> String{
     return fs::read_to_string(path).unwrap();
 }
 
-fn find_calibration_value(line: &str) -> u32 {
-    // Find first digit.
-    let first_digit = line.chars()
+fn find_first_digit(string: &str) -> u32{
+    string.chars()
         .find(|a| a.is_digit(10))
         .and_then(|a| a.to_digit(10))
-        .unwrap();
+        .unwrap()
+}
 
-    // Find last digit.
-    let reverse_line = line.chars().rev().collect::<String>();
-    let last_digit = reverse_line.chars()
-        .find(|a| a.is_digit(10))
-        .and_then(|a| a.to_digit(10))
-        .unwrap();
+fn find_calibration_value(line: &str) -> u32 {
+    let first_digit = find_first_digit(line);
+
+    let last_digit = find_first_digit(
+        &line.chars().rev().collect::<String>()     // reverse line
+    );
 
     (first_digit * 10 + last_digit) as u32
 }
 
-fn find_first_digit(string: &str){
-    string.chars().find(|a| a.is_digit(10))
-        .and_then(|a| a.to_digit(10))
-        .unwrap();
-}
 
-
-
-fn find_calibration_value_part2(line: &str) -> u32 {
-
+fn find_true_calibration_value(line: &str) -> u32 {
     let numbers:HashMap<&str, u32> = HashMap::from([
         ("zero", 0), ("one", 1), ("two", 2), ("three", 3),
         ("four", 4), ("five", 5), ("six", 6), ("seven", 7),
         ("eight", 8), ("nine", 9),
     ]);
 
-    let mut first_digit:(usize, u32) = (line.len(), 0);
-    let mut last_digit = (0, 0);
-    for (i,c) in line.chars().enumerate(){
+    // find first and last occurrence of a number as a digit or word
+    let mut first_digit = (line.len(), 0);  // (index, value)
+    let mut last_digit = (0, 0);            // (index, value)
+    for (index,_c) in line.chars().enumerate(){
         for (word, value) in numbers.iter(){
-            if line[i..].starts_with(word) || line[i..].starts_with(char::from_digit(*value, 10).unwrap()) {
-                if i < first_digit.0 {
-                    first_digit = (i, *value)
+            if line[index..].starts_with(word)
+                || line[index..].starts_with(char::from_digit(*value, 10).unwrap()) {
+                if index < first_digit.0 {
+                    first_digit = (index, *value)
                 }
-                else if i > last_digit.0 {
-                    last_digit = (i, *value)
+                if index >= last_digit.0 {
+                    last_digit = (index, *value)
                 }
             }
         }
@@ -72,9 +66,7 @@ pub fn solve_part2() -> u32 {
 
     let mut sum = 0;
     for line in input.lines(){
-        sum += find_calibration_value_part2(line);
+        sum += find_true_calibration_value(line);
     }
     sum
 }
-
-
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod day01;
 
 fn main() {
     let sum = day01::solve_part1();
-    println!("result {}", sum);
+    println!("result day1 part 1 {}", sum);
     let sum = day01::solve_part2();
-    println!("result {}", sum);
+    println!("result day 1 part 2 {}", sum);
 }
