b76325aa0c0ed92138e2cab9a071b9688759f5f4
Elena Jahr <ejahr@protonmail.com>
2023-12-03 08:53:31 +0100

    solve day 1 part 2

--- src/day01.rs
+++ src/day01.rs
@@ -1,4 +1,5 @@
 use std::fs;
+use std::collections::HashMap;
 
 fn read_input_to_string() -> String{
     let path = "input/day01.txt";
@@ -22,6 +23,40 @@ fn find_calibration_value(line: &str) -> u32 {
     (first_digit * 10 + last_digit) as u32
 }
 
+fn find_first_digit(string: &str){
+    string.chars().find(|a| a.is_digit(10))
+        .and_then(|a| a.to_digit(10))
+        .unwrap();
+}
+
+
+
+fn find_calibration_value_part2(line: &str) -> u32 {
+
+    let numbers:HashMap<&str, u32> = HashMap::from([
+        ("zero", 0), ("one", 1), ("two", 2), ("three", 3),
+        ("four", 4), ("five", 5), ("six", 6), ("seven", 7),
+        ("eight", 8), ("nine", 9),
+    ]);
+
+    let mut first_digit:(usize, u32) = (line.len(), 0);
+    let mut last_digit = (0, 0);
+    for (i,c) in line.chars().enumerate(){
+        for (word, value) in numbers.iter(){
+            if line[i..].starts_with(word) || line[i..].starts_with(char::from_digit(*value, 10).unwrap()) {
+                if i < first_digit.0 {
+                    first_digit = (i, *value)
+                }
+                else if i > last_digit.0 {
+                    last_digit = (i, *value)
+                }
+            }
+        }
+    }
+
+    (first_digit.1 * 10 + last_digit.1) as u32
+}
+
 pub fn solve_part1() -> u32 {
     let input = read_input_to_string();
 
@@ -32,4 +67,13 @@ pub fn solve_part1() -> u32 {
     sum
 }
 
+pub fn solve_part2() -> u32 {
+    let input = read_input_to_string();
+
+    let mut sum = 0;
+    for line in input.lines(){
+        sum += find_calibration_value_part2(line);
+    }
+    sum
+}
 
--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,6 @@ mod day01;
 fn main() {
     let sum = day01::solve_part1();
     println!("result {}", sum);
+    let sum = day01::solve_part2();
+    println!("result {}", sum);
 }
