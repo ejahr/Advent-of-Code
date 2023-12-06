e17ac5c5ef1dad9f54126556f9309fe7dcbc6cb1
jahre <ejahr@protonmail.com>
2023-12-06 21:17:27 +0100

    solve day 5 part 2 multithreaded

--- src/day05.rs
+++ src/day05.rs
@@ -1,56 +1,21 @@
-/* Using the maps, which describe how to convert numbers from a source category
-into numbers in a destination category, find the lowest location number that
-corresponds to any of the initial seeds.
+/* Find the lowest location number that corresponds to any of the initial seeds.
 To do this, you'll need to convert each seed number through other
 categories until you can find its corresponding location number.
-Solution part 1:  165788812
-Solution part 2:
+The values on the initial seeds: line come in pairs. Within each pair, the first
+value is the start of the range and the second value is the length of the range.
+Solution part 1: 165788812
+Solution part 2: 1928058
  */
 
 use std::fs;
+use std::thread;
+use std::thread::JoinHandle;
 
 fn read_input() -> (Vec<i64>, Vec<Vec<Vec<i64>>>) {
     let path = "input/day05.txt";
-
-
-    let input = "seeds: 79 14 55 13
-
-seed-to-soil map:
-50 98 2
-52 50 48
-
-soil-to-fertilizer map:
-0 15 37
-37 52 2
-39 0 15
-
-fertilizer-to-water map:
-49 53 8
-0 11 42
-42 0 7
-57 7 4
-
-water-to-light map:
-88 18 7
-18 25 70
-
-light-to-temperature map:
-45 77 23
-81 45 19
-68 64 13
-
-temperature-to-humidity map:
-0 69 1
-1 0 69
-
-humidity-to-location map:
-60 56 37
-56 93 4";
     let input = fs::read_to_string(path).unwrap();
 
-    //let (first_line, remainder) = input.split_once("\n\n").unwrap();
     let (first_line, remainder) = input.split_once("\r\n\r\n").unwrap();
-
     let seeds: Vec<i64> = get_seeds(first_line);
     let maps: Vec<Vec<Vec<i64>>> = get_maps(remainder);
 
@@ -66,18 +31,15 @@ fn get_seeds(first_line: &str) -> Vec<i64> {
 
 fn get_maps(remainder: &str) -> Vec<Vec<Vec<i64>>> {
     let mut maps: Vec<Vec<Vec<i64>>> = Vec::new();
-
     // iterate over individual maps
     for map_as_string in remainder.split("\r\n\r\n"){
-    //for map_as_string in remainder.split("\n\n"){
         let mut map_as_vec: Vec<Vec<i64>> = Vec::new();
-
         // iterate only over lines containing numbers
         for line in map_as_string.split_once(":\r\n").unwrap().1.lines() {
-        //for line in map_as_string.split_once(":\n").unwrap().1.lines() {
-            map_as_vec.push( line.split_whitespace()
-                .map(|m| { m.parse::<i64>().unwrap() })
-                .collect::<Vec<i64>>()
+            map_as_vec.push(
+                line.split_whitespace()
+                    .map(|m| { m.parse::<i64>().unwrap() })
+                    .collect::<Vec<i64>>()
             );
         }
         maps.push(map_as_vec);
@@ -86,46 +48,28 @@ fn get_maps(remainder: &str) -> Vec<Vec<Vec<i64>>> {
 }
 
 fn convert(number: &i64, map: &Vec<Vec<i64>>) -> i64 {
-    for sub_map in map{
-        if (number >= &sub_map[1]) & (number <= &(sub_map[1] + sub_map[2])) {
+    for sub_map in map {
+        if (number >= &sub_map[1]) && (number <= &(sub_map[1] + sub_map[2])) {
             return number + sub_map[0] - sub_map[1]
         }
     }
     *number
 }
 
-fn get_location_of_seeds(seeds: Vec<i64>, maps: Vec<Vec<Vec<i64>>> ) -> Vec<i64>{
-    let mut converted_seeds = Vec::new();
-
-    for seed in seeds.iter(){
-        let mut converted_seed = *seed;
-        for map in maps.iter() {
-            converted_seed = convert(&converted_seed, map);
-        }
-        converted_seeds.push(converted_seed);
+fn get_location_of_seed(seed: i64, maps: &Vec<Vec<Vec<i64>>>) -> i64 {
+    let mut converted_seed = seed;
+    for map in maps.iter() {
+        converted_seed = convert(&converted_seed, map);
     }
-
-    converted_seeds
+    converted_seed
 }
-
-fn get_seeds_from_ranges(seed_ranges: Vec<i64>) -> Vec<i64>{
-    let mut seeds = Vec::new();
-    //println!("seed_ranges {:?}", seed_ranges);
-    for i in (0..seed_ranges.len()).step_by(2){
-        //println!("i {}", i);
-        for s in seed_ranges[i]..seed_ranges[i]+seed_ranges[i+1]{
-            seeds.push(s);
-        }
-    }
-    //println!("seeds {:?}", seeds);
-    seeds
-}
-
 
 pub fn solve_part1() -> i64 {
     let (seeds, maps) = read_input();
 
-    let locations = get_location_of_seeds(seeds, maps);
+    let locations =  seeds.iter()
+        .map( |i| get_location_of_seed(i.clone(), &maps) )
+        .collect();
 
     *locations.iter().min().unwrap()
 }
@@ -133,9 +77,26 @@ pub fn solve_part1() -> i64 {
 pub fn solve_part2() -> i64 {
     let (seed_ranges, maps) = read_input();
 
-    let seeds = get_seeds_from_ranges(seed_ranges);
+    let mut threads: Vec<JoinHandle<i64>> = Vec::new();
+    for i in (0..seed_ranges.len()).step_by(2) {
+        let seed_range_copy = seed_ranges.clone();
+        let maps_copy = maps.clone();
+        threads.push(thread::spawn(move || {
+            let mut min_location = i64::MAX;
+            println!("start {i}");
+            for seed in seed_range_copy[i]..seed_range_copy[i] + seed_range_copy[i + 1] {
+                let location = get_location_of_seed(seed, &maps_copy);
+                if location < min_location {
+                    min_location = location;
+                }
+            }
+            println!("end {i}");
 
-    let locations = get_location_of_seeds(seeds, maps);
+            min_location
+        }))
+    }
 
-    *locations.iter().min().unwrap()
+    threads.into_iter()
+        .map(|m| m.join().unwrap())
+        .min().unwrap()
 }
